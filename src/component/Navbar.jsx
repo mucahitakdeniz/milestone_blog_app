dd6d7b853affcf4e9ab67cf2506ac791d7d88855
Mücahit Akdeniz <mucahitakdeniz28@gmail.comgit config --global user.email mucahitakdeniz28@gmail.com>
2023-08-23 15:21:38 +0300

    navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,9 +3,6 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-// import Switch from "@mui/material/Switch";
-// import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import { useState } from "react";
@@ -29,72 +26,80 @@ const Navbar = () => {
 
   return (
     <Box sx={{ with: "100%" }}>
-      <FormGroup></FormGroup>
       <AppBar
         position="static"
         sx={{ with: "100%", height: "5rem", backgroundColor: "lightseagreen" }}
       >
-        <Toolbar sx={{ alignItems: "center" }}>
-          <Link
-            color="#01020c"
-            variant="button"
-            underline="none"
-            marginX={1}
-            href="/"
-          >
-            Dashboard
-          </Link>
-          <Link
-            color="#01020c"
-            variant="button"
-            underline="none"
-            marginX={1}
-            href="/register"
-          >
-            New Blog
-          </Link>
-          <Link
-            color="#01020c"
-            variant="button"
-            underline="none"
-            marginX={1}
-            href="/login"
-          >
-            About
-          </Link>
-
-          {auth && (
-            <div sx={{marginLeft:"auto"}}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
+        <Toolbar sx={{display:"flex",   justifyContent:"space-between"}}>
+          <Box>
+            <img
+              src="https://lms.clarusway.com/pluginfile.php/1/core_admin/logocompact/300x300/1688763931/clarusway_LOGO_tek_png.png"
+              width="50rem"
+              height="50rem"
+              alt="logo"
+            />
+            <Link
+              color="#01020c"
+              variant="button"
+              underline="none"
+              marginX={1}
+              href="/"
+            >
+              Dashboard
+            </Link>
+            <Link
+              color="#01020c"
+              variant="button"
+              underline="none"
+              marginX={1}
+              href="/register"
+            >
+              New Blog
+            </Link>
+            <Link
+              color="#01020c"
+              variant="button"
+              underline="none"
+              marginX={1}
+              href="/login"
+            >
+              About
+            </Link>
+          </Box>
+          <Box>
+            {auth && (
+              <div>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
