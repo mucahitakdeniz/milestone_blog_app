e42233f66d4433b46b2ebce3f36c9e1e9be40044
Mücahit Akdeniz <mucahitakdeniz28@gmail.comgit config --global user.email mucahitakdeniz28@gmail.com>
2023-08-24 16:34:50 +0300

    cards

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -4,17 +4,39 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
-import { minWidth } from "@mui/system";
-import { spacing } from "@mui/system";
+import { Box, Grid } from "@mui/material";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import { useNavigate } from "react-router-dom";
 const Cards = ({ cardsData }) => {
+  const currentUser = false;
   console.log(cardsData);
+  const navigate = useNavigate();
+  const handleMore = () => {
+    navigate("/readmore");
+  };
 
   return (
-    <Grid container spacing={20} sx={{marginY:"2rem",justifyContent:"center"}}>
+    <Grid
+      container
+      spacing={5}
+      sx={{ marginY: "2rem", justifyContent: "center" }}
+    >
       {cardsData.map((item) => (
-        <Grid item key={item.id} spacing={5} xs={12} sm={6}  md={4}  >
-          <Card sx={{padding:"2rem", }}>
+        <Grid item key={item.id} xs={12} sm={6} md={4}>
+          <Card
+            sx={{
+              padding: "2rem",
+              width: "80%",
+              height: "30rem",
+              boxShadow: "0 10px 18px rgba(3, 2, 2, 0.788)",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              gap: "5px",
+            }}
+          >
             <CardMedia
               sx={{ height: "100px", width: "100px", margin: "auto" }}
               image={item.image}
@@ -32,10 +54,48 @@ const Cards = ({ cardsData }) => {
               >
                 {item.content}
               </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {new Intl.DateTimeFormat("tr-TR", {
+                  year: "numeric",
+                  month: "2-digit",
+                  day: "2-digit",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  second: "2-digit",
+                  hour12: false,
+                }).format(new Date(item.publish_date))}
+              </Typography>
+              <Typography variant="body3" color="text.secondary">
+                {item.author}
+              </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
+              <Box sx={{ display: "flex", gap: "5px" }}>
+                <Box sx={{ display: "flex" }}>
+                  <FavoriteIcon />{" "}
+                  <Typography variant="body3" color="text.secondary">
+                    {item.likes}
+                  </Typography>
+                </Box>
+                <Box sx={{ display: "flex" }}>
+                  <ChatBubbleOutlineIcon />
+                  <Typography variant="body3" color="text.secondary">
+                    {item.comment_count}
+                  </Typography>
+                </Box>
+                <Box sx={{ display: "flex" }}>
+                  <RemoveRedEyeIcon />
+                  <Typography variant="body3" color="text.secondary">
+                    {item.post_views}
+                  </Typography>
+                </Box>
+              </Box>
+              <Button
+                sx={{ backgroundColor: "lightgreen", marginLeft: "2rem" }}
+                onClick={() => handleMore(item.id)}
+              >
+                Read More
+              </Button>
             </CardActions>
           </Card>
         </Grid>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,16 +5,19 @@ import Home from "../pages/Home";
 import Dishboard from "../component/Dishboard";
 import About from "../component/About";
 import NewBlog from "../component/NewBlog";
+import ReadMore from "../component/ReadMore";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/" element={<Home />} >
-        <Route index element={<Dishboard/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/newblog" element={<NewBlog/>} />
+      <Route path="/" element={<Home />}>
+        <Route index element={<Dishboard />} />
+        <Route path="/readmore" element={<ReadMore />} />
+
+        <Route path="/about" element={<About />} />
+        <Route path="/newblog" element={<NewBlog />} />
       </Route>
     </Routes>
   );
