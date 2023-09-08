3ce33052f5cd6b0d462561b41be96f17fc8145fd
mucahit <mucahitakdeniz28@gmail.com>
2023-09-08 17:20:31 +0300

    readMore

--- src/hooks/useCardsFn.jsx
+++ src/hooks/useCardsFn.jsx
@@ -1,17 +1,21 @@
 import { fetchFail, fetchStart, readCards } from "../features/cardsSlice";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 
 const useCardsFn = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
 
   const readMore = async (id) => {
     dispatch(fetchStart());
     try {
       const { data } = await axios.get(
-        `${import.meta.env.VITE_BASE_URL}/api/blogs/${id}/`
+        `${import.meta.env.VITE_BASE_URL}/api/blogs/${id}/`,
+        {
+          headers: { Authorization: `Token ${token}` },
+        }
       );
       dispatch(readCards(data));
       console.log(data);
@@ -20,7 +24,10 @@ const useCardsFn = () => {
       console.log(error);
       dispatch(fetchFail());
     }
+    finally{
+      console.log(token);
+    }
   };
-  return{readMore}
+  return { readMore };
 };
 export default useCardsFn;
