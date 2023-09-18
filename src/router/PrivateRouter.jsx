6f211a2d4bd11cea88c4ff7c77632bc0f8f450f8
Mücahit Akdeniz <mucahitakdeniz28@gmail.comgit config --global user.email mucahitakdeniz28@gmail.com>
2023-09-18 11:31:17 +0300

    priveteRouter

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,17 +1,19 @@
-import {useNavigate } from "react-router-dom";
+import {Navigate} from "react-router-dom";
 import NewBlog from "../pages/NewBlog";
 import { useSelector } from "react-redux";
+import { notify } from "../helper/sweetaAlert";
 
 const PrivateRouter = () => {
   const { currentUser } = useSelector((state) => state.auth);
-  const navigate =  useNavigate() 
 
-  if (currentUser) {
-    return <NewBlog />;
-  } else {
+  if (!currentUser) {
     notify("You must be logged in to use this feature", "error");
-    navigate("/login");
-  }
-};
+    return <Navigate to="/login" />;
+}
+
+
+    return <NewBlog />
+}
+  
 
 export default PrivateRouter;
